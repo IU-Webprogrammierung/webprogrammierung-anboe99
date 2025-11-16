87f32eb06e9e5b33775ed866d5f324568f49fa7b
Andreas Böhm <andiboehm99@icloud.com>
2025-11-16 11:49:57 +0100

    feat: additional hover effect
    added a shine following the users mouse cursor when hovering on buttons

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,25 @@ const observer = new IntersectionObserver((entries) => {
 const elementsToAnimate = document.querySelectorAll('.fade-in-on-scroll');
 
 elementsToAnimate.forEach((el) => observer.observe(el));
+
+
+
+// Shine-Effect on Buttons
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttonShine = document.querySelectorAll('.button-primary');
+    buttonShine.forEach(button => {
+        button.addEventListener('mousemove', (e) => {
+            const rect = button.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            button.style.setProperty('--mouse-x', `${x}px`);
+            button.style.setProperty('--mouse-y', `${y}px`);
+        });
+        button.addEventListener('mouseleave', () => {
+            button.style.setProperty('--mouse-x', 'center');
+            button.style.setProperty('--mouse-y', 'center');
+        });
+    });
+
+});
